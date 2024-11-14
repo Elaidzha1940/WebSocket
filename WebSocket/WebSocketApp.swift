985c600f43c74fec968ec2f6bf4a3ca06fa71297
Elaidzha1940 <eli080293@gmail.com>
2024-11-14 05:52:44 +0300

    WebSocket

---
+++ WebSocket/WebSocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebSocketApp.swift
+//  WebSocket
+//
+//  Created by Elaidzha Shchukin on 14.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct WebSocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
